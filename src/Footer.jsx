51fc254c999c4527ccc0678c6e6b8b8b2e26fcc3
Ashis28 <dashashiskumar28@example.com>
2025-07-09 15:57:52 +0530

    Initial commit! with .gitignore,app,food,footer jsx files

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return (
+        <footer>
+            <p>Copyright 2025</p>
+            <p>&copy; {new Date().getFullYear()}copyright strike</p>
+        </footer>
+    )
+}
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <header>
+            <h1>my react app</h1>
+            <p><a href="#">Home</a></p>
+            <p><a href="#">About</a></p>
+            <p><a href="#">Contact</a></p>
+        </header>
+    );
+}
+
+export default Header;
